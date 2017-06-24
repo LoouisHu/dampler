e64dc1b403ef0000472a30dce9de21bca97c5409
s1530003 <l.hu@student.utwente.nl>
2017-06-24 12:37:22 +0200

    forgot to add, now it is added

---
+++ src/DamplerGenerator.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Noble Notebook Louis on 24-Jun-17.
+ */
+public class DamplerGenerator {
+}
---
+++ src/TypeKind.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Noble Notebook Louis on 24-Jun-17.
+ */
+public enum TypeKind {
+    /** Integer base type. */
+    INT,
+    /** Boolean base type. */
+    BOOL
+}
